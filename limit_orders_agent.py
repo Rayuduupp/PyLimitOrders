49687e7d60078dd9aa71ff085d1350173c0f034a
Rayuduupp <roy.muthyala@gmail.com>
2024-08-11 23:23:16 +0530

    Update limit_orders_agent.py

--- limit_orders_agent.py
+++ limit_orders_agent.py
@@ -1,5 +1,12 @@
-from trading_framework.execution_client import ExecutionClient
-from trading_framework.price_listener import PriceListener
+from execution_client import ExecutionClient,ExecutionException
+from price_listener import PriceListener
+
+held_count=0
+product_id="IB"
+price=[60,50,40,30,55]
+boundary_limit=50
+amount=5000
+buy_flag='Y'
 
 
 class LimitOrderAgent(PriceListener):
@@ -10,63 +17,77 @@ class LimitOrderAgent(PriceListener):
         :param execution_client: can be used to buy or sell - see ExecutionClient protocol definition
         """
         self.execution_client=execution_client
+        self.product_id=product_id
+        self.price=price
+        
         super().__init__()
 
-    def buy(self, product_id: str, amount: int):
-        """
-        Execute a buy order, throws ExecutionException on failure
-        :param product_id: the product to buy
-        :param amount: the amount to buy
-        :return: None
-        """
-        
-        number_of_orders_bought=0
-        number_of_orders_bought=(amount/current_price)
-        if number_of_orders_bought==0:
-            raise Exception("cannot buy orders")
-        return number_of_orders_bought
-        
-
-    def sell(self, product_id: str, amount: int):
-        """
-        Execute a sell order, throws ExecutionException on failure
-        :param product_id: the product to sell
-        :param amount: the amount to sell
-        :return: None
-        """
-        number_of_sold_orders=0
-        number_of_sold_orders=(amount/current_price)
-        if number_of_sold_orders==0:
-            raise Exception("cannot sell orders")
-        return number_of_sold_orders
+    
+       
 
     def on_price_tick(self, product_id: str, price: float):
         # see PriceListener protocol and readme file
-        print("The current market price of",self.product_id,"is",self.price)
-        current_price=self.price
-        return self.price
+        print("The current market price of",product_id,"is",price)
+        current_price=price
+        return current_price
     
     
 
-objectdata=LimitOrderAgent()
+objectdata=LimitOrderAgent(ExecutionClient)
 
+
+           
+#Function for adding new orders
 def add_order(flag,product_id,amount,boundary_limit):
-        if flag=='Y':
-            current_price=self.price
-            if current_price<100:
-        
-                objectdata.buy("IBM",current_price)
+        for i in range(len(price)-1):
+            if price[i]!=price[i+1]:
+                
+                
+                global current_price
+                current_price=objectdata.on_price_tick(product_id,price[i])
+            try:
+                
+                if flag=='Y':
             
-        elif flag=='N':
-            current_price=self.price
-            if current_price<100:
-                objectdata.sell("IBM",current_price)
+                    if current_price<boundary_limit and current_price<100:
 
-def held_orders(held_orders,buy_sell_flag,product_id,amount):
+                        objectdata.execution_client.buy(product_id,amount,amount)
     
-    if held_orders>=1:
-        
-        objectdata.add_order(buy_sell_flag,product_id,amount,boundary_limit)
-
+                    #objectdata.buy(product_id,amount)
+                
+                    else:
+                        print( "Order cannot be bought as the Price value is greater than 100 or outside the limit")
             
+                elif flag=='N':
+            
+                    if current_price>=boundary_limit:
+                
+                        objectdata.execution_client.sell(product_id,amount,amount)
+                
+            except ExecutionException as e:
+                        print(f"Order execution failed: {e}")
+            
+                    
+
+#Function to check held orders in queue
+
+
+def held_orders(held_count,buy_sell_flag,product_id,amount,boundary_limit):
+    
+           
+        add_order(buy_sell_flag,product_id,amount,boundary_limit)
+
+
+if held_count>=1:
+    print("This is a held order\n")
+    held_orders(held_count,buy_flag,product_id,amount,boundary_limit)
+else:
+    print("This is a direct order\n")
+    add_order(buy_flag,product_id,amount,boundary_limit)
+    
+
+
+
+    
+  
     
